713c595c8d544b000c3797c072f650d833fbdeb7
Aaron Sanchez <saarong12@gmail.com>
2023-11-10 00:43:12 -0600

    Created a map that has every month as the key and the value is a slice of all the birthdays that occur in that month

--- main.go
+++ main.go
@@ -10,21 +10,17 @@ import (
 	"time"
 )
 
-func getBirthdays() map[int]int {
-	// Open the file we want to read
+func getBirthdays() (map[int]int, map[string][]string) {
 	file, err := os.Open("birthdays.csv")
 	if err != nil {
 		fmt.Println("File could not be opened", err)
 	}
 	defer file.Close()
 
-	// create a new buffered reader
 	reader := bufio.NewReader(file)
 
-	// Creating a slice and add all the birthdays to be looped through
 	var birthdays []string
 
-	// Infinite loop that loops through the file looking for new lines and prints the lines unless it is the EOF (end of file) or we get an err
 	for {
 		line, err := reader.ReadString('\n')
 
@@ -41,46 +37,66 @@ func getBirthdays() map[int]int {
 
 	bdayCount := make(map[int]int)
 
-	// Looping through the created birthdays slice
+	monthWithBirthdays := map[string][]string{
+		"January":   {},
+		"February":  {},
+		"March":     {},
+		"April":     {},
+		"May":       {},
+		"June":      {},
+		"July":      {},
+		"August":    {},
+		"September": {},
+		"October":   {},
+		"November":  {},
+		"December":  {},
+	}
+
+	// fmt.Println(monthWithBirthdays)
+
 	for _, val := range birthdays {
 
-		// Seperates the month from the rest of the date
 		month := strings.Split(val, "/")
-		// fmt.Println(month[0])
 
-		// Converts the month string into an integer using the strconv package
 		monthInt, err := strconv.Atoi(month[0])
 		if err != nil {
-			// fmt.Println("There was a  conversiong problem", err)
 			continue
 		}
 
-		// Check to see if the month exists in the csv file and if it does add one to its value
 		if value, ok := bdayCount[monthInt]; ok {
 			bdayCount[monthInt] = value + 1
 		} else {
 			bdayCount[monthInt] = 1
 		}
+
+		monthStr := time.Month(monthInt)
+
+		// fmt.Println(monthWithBirthdays[monthStr.String()])
+		if slice, ok := monthWithBirthdays[monthStr.String()]; ok {
+			monthWithBirthdays[monthStr.String()] = append(slice, val)
+		}
+
 	}
 
-	return bdayCount
+	return bdayCount, monthWithBirthdays
 
 }
 
 func main() {
 
-	birthdays := getBirthdays()
+	birthdays, monthsWithBirthdays := getBirthdays()
+
+	fmt.Println(monthsWithBirthdays)
 
 	commands := map[string]string{
-		"[help]": "- display a list of commands",
-		"[most]": "- display the month with the most birthdays",
+		"[help]": "- Display a list of commands",
+		"[most]": "- Display the month with the most birthdays",
 		"[exit]": "- End the program",
 	}
 
 	var userInput string
 	loop := true
 
-	// This is the cli loop where the programs askes for user input
 	fmt.Println("Welcome to the birthday metrics CLI tool")
 	fmt.Println("For a list of commands please type [help]")
 
@@ -103,7 +119,6 @@ func main() {
 	}
 }
 
-// takes the map of birhtdays and loops through to find the month with the most birthdays via its value
 func mostBirthdays(birthdays map[int]int) string {
 	var keyPlaceholder, largest int
 	for key, value := range birthdays {
